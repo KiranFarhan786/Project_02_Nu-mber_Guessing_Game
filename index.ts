cfdc52a4d39c122a19c8b656e097233401a7cc60
KiranFarhan786 <167645632+KiranFarhan786@users.noreply.github.com>
2024-05-27 18:22:55 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let roundNumber: number = Math.floor(( Math.random() * 5) + 1);
+
+let userInput = await inquirer.prompt({
+    type: "number",
+    name: "guessedNumber",
+    message: "Guess a number between 1 -5",
+
+});
+
+if(userInput.guessedNumber === roundNumber){
+    console.log("congratulation! you guessed correctly");
+}else{
+    console.log(`sorry! try next time. correct answer is: ${roundNumber}`);
+};
